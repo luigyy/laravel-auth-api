0414b52bbad01badde72515b0df26ea8eeb18f2b
luigyy <luigy.valverde@ucr.ac.cr>
2023-03-13 17:01:48 -0600

    added login route

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -32,7 +34,7 @@ class AuthController extends Controller
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
-                'password' => $request->password,
+                'password' => Hash::make($request->password),
             ]);
             return response()->json([
                 'status' => true,
@@ -46,7 +48,47 @@ class AuthController extends Controller
             ], 500);
         }
     }
-    public function loginUser()
+    public function loginUser(Request $request)
     {
+        try {
+            //validate data
+            $validateUser = Validator::make($request->all(), [
+                "email" => "email|required",
+                "password" => "required"
+            ]);
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors(),
+                ]);
+            }
+
+            //check if authentication fails
+            if (!Auth::attempt($request->only([
+                'email', 'password'
+            ]))) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'authentication failed',
+                ], 401);
+            }
+
+            //auth successful => get user from db 
+            $user = User::where("email", $request->email)->first();
+
+            // return success with authorization token 
+            return response()->json([
+                'status' => true,
+                'message' => 'Logged in successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken,
+            ], 200);
+            //
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 }
